e4cad353fdde973c6c8124f4b30b37efe064505e
theogaditya <theogadityahota@gmail.com>
2025-08-21 11:54:54 +0530

    added env

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const dotenv = require('dotenv');
+dotenv.config();
 
 const port = 3000;
+const databaseUrl = process.env.DATABASE_URL;
+const cors = process.env.cors;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send(`Hello World! 
+    my db: ${databaseUrl}
+    my cors policy: ${cors}`); 
+
 });
 
 app.listen(port, () => {
